efcde2d0dce0d2692ccbea28f71b4dcb7f332bb7
Jonatas Cavalcante <jonatasben@hotmail.com>
2025-11-10 14:51:56 -0300

    Remove linhas em branco desnecessárias em calculodePecas e multiplosEntresi

--- Programas/src/calculodePecas.java
+++ Programas/src/calculodePecas.java
@@ -6,10 +6,8 @@ public class calculodePecas {
             System.out.println("Vamos descobrir o valor do produto");
             System.out.println("Digite o codigo da peça");
             int peca = scan.nextInt();
-
             System.out.println("Digite a quantidade de peças");
             int quantidade = scan.nextInt();
-
             System.out.println("Digite o valor unitario de cada peça");
             double valor = scan.nextDouble();
             System.out.println("Vamos repetir o processo");
@@ -19,7 +17,6 @@ public class calculodePecas {
             
             System.out.println("Digite a quantidade de peças");
             int quantidade2 = scan.nextInt();
-
             System.out.println("Digite o valor unitario de cada peça");
             double valor2 = scan.nextDouble();
 
--- Programas/src/multiplosEntresi.java
+++ Programas/src/multiplosEntresi.java
@@ -4,7 +4,6 @@ public class multiplosEntresi {
                 public static void main(String[] args) {
                  Scanner scan = new Scanner(System.in);   
                     System.out.println("Vamos descobrir se os números são mútiplos entre si");
-
                     int num1, num2;
                     System.out.println("Digite o primeiro número inteiro");
                     num1 = scan.nextInt();
@@ -15,7 +14,6 @@ public class multiplosEntresi {
                         System.out.println("Esses números são múltiplos ");
                     }else {
                         System.out.println("Esses números não são múltiplos");}
-
                 scan.close();
 
                 }
